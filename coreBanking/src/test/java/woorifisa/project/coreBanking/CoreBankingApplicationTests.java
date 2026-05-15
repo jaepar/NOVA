89affd9f2cbdf02c0479384f4a2935646d30af29
Jay Park <pjh6183@naver.com>
2026-05-15 14:24:43 +0900

    coreBanking 서버 init

---
+++ coreBanking/src/test/java/woorifisa/project/coreBanking/CoreBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package woorifisa.project.coreBanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
