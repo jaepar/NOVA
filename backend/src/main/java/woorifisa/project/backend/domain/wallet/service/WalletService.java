204a9fdaa096394f8750eece4727ba641456677f
jyu <jiwoo061@gmail.com>
2026-05-23 14:01:39 +0900

    feat: 월렛 계좌내역 조회 API 엔드포인트 추가

--- backend/src/main/java/woorifisa/project/backend/domain/wallet/service/WalletService.java
+++ backend/src/main/java/woorifisa/project/backend/domain/wallet/service/WalletService.java
@@ -2,8 +2,29 @@ package woorifisa.project.backend.domain.wallet.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import woorifisa.project.backend.domain.wallet.dto.response.WalletTransactionsResponse;
+import woorifisa.project.backend.domain.wallet.entity.Wallet;
+import woorifisa.project.backend.domain.wallet.entity.WalletTransaction;
+import woorifisa.project.backend.domain.wallet.repository.WalletRepository;
+import woorifisa.project.backend.domain.wallet.repository.WalletTransactionRepository;
+import woorifisa.project.backend.global.exception.CustomException;
+
+import java.util.List;
+
+import static woorifisa.project.backend.global.response.status.BaseExceptionResponseStatus.WALLET_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
 public class WalletService {
+
+    private final WalletRepository walletRepository;
+    private final WalletTransactionRepository walletTransactionRepository;
+
+    public WalletTransactionsResponse findWalletTransactions(Long userId) {
+        Wallet wallet = walletRepository.findByUser_UserId(userId)
+                .orElseThrow(() -> new CustomException(WALLET_NOT_FOUND));
+        List<WalletTransaction> transactions = walletTransactionRepository.findAllByWallet_WalletIdOrderByCreatedAtDesc(wallet.getWalletId());
+
+        return WalletTransactionsResponse.from(wallet, transactions);
+    }
 }
--- backend/src/main/java/woorifisa/project/backend/global/response/status/BaseExceptionResponseStatus.java
+++ backend/src/main/java/woorifisa/project/backend/global/response/status/BaseExceptionResponseStatus.java
@@ -15,7 +15,12 @@ public enum BaseExceptionResponseStatus implements ResponseStatus {
      */
     INVALID_PASSWORD_FORMAT("AUTH-001", "비밀번호 형식이 올바르지 않습니다."),
     PASSWORD_CONFIRM_NOT_MATCHED("AUTH-002", "비밀번호와 비밀번호 확인이 일치하지 않습니다."),
-    DUPLICATE_EMAIL("AUTH-003", "이미 가입된 이메일입니다.");
+    DUPLICATE_EMAIL("AUTH-003", "이미 가입된 이메일입니다."),
+
+    /*
+     * wallet : 400
+     */
+    WALLET_NOT_FOUND("WALLET-001", "월렛 정보를 찾을 수 없습니다.");
 
     private final boolean success = false;
     private final String code;
