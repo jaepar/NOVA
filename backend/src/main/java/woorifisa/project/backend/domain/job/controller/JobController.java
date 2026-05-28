dbdcc814972fa517aa9dec4d9ef30d49ec4e0363
백민정 <ggamji09@naver.com>
2026-05-28 15:27:23 +0900

    feat: 구인구직 목록 조회 API 엔드포인트 추가

--- backend/src/main/java/woorifisa/project/backend/domain/job/controller/JobController.java
+++ backend/src/main/java/woorifisa/project/backend/domain/job/controller/JobController.java
@@ -1,11 +1,26 @@
 package woorifisa.project.backend.domain.job.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import woorifisa.project.backend.domain.job.dto.response.JobOpeningListResponse;
+import woorifisa.project.backend.domain.job.service.JobService;
+import woorifisa.project.backend.global.response.BaseResponse;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/jobs")
 public class JobController {
+
+    private final JobService jobService;
+
+    @GetMapping
+    public BaseResponse<JobOpeningListResponse> findJobOpenings(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        return BaseResponse.ok(jobService.findJobOpenings(page, size));
+    }
 }
