9d724d517891e2f78d0bd23782f7e1744f5701ea
WOOJAE JO <whgjsgh11@gmail.com>
2026-05-29 14:30:13 +0900

    feat: 계좌 생성 Controller

--- coreBanking/src/main/java/woorifisa/project/coreBanking/domain/account/controller/AccountController.java
+++ coreBanking/src/main/java/woorifisa/project/coreBanking/domain/account/controller/AccountController.java
@@ -1,13 +1,16 @@
 package woorifisa.project.coreBanking.domain.account.controller;
 
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import woorifisa.project.coreBanking.domain.account.dto.request.AccountPasswordVerifyRequest;
+import woorifisa.project.coreBanking.domain.account.dto.request.CreateAccountRequest;
 import woorifisa.project.coreBanking.domain.account.dto.request.RecipientLookupRequest;
+import woorifisa.project.coreBanking.domain.account.dto.response.CreateAccountResponse;
 import woorifisa.project.coreBanking.domain.account.dto.response.RecipientLookupResponse;
 import woorifisa.project.coreBanking.domain.account.service.AccountService;
 import woorifisa.project.coreBanking.global.response.BaseResponse;
@@ -17,22 +20,30 @@ import woorifisa.project.coreBanking.global.response.BaseResponse;
 @RequestMapping("/accounts")
 public class AccountController {
 
-    private final AccountService accountService;
+	private final AccountService accountService;
 
-    // 수취인 조회
-    @PostMapping("/recipients/lookup")
-    public BaseResponse<RecipientLookupResponse> lookupRecipient(
-            @Valid @RequestBody RecipientLookupRequest request
-    ) {
-        return BaseResponse.ok(accountService.lookupRecipient(request));
-    }
+	// 수취인 조회
+	@PostMapping("/recipients/lookup")
+	public BaseResponse<RecipientLookupResponse> lookupRecipient(
+		@Valid @RequestBody RecipientLookupRequest request
+	) {
+		return BaseResponse.ok(accountService.lookupRecipient(request));
+	}
 
-    // 계좌 비밀번호 검증
-    @PostMapping("/password/verify")
-    public BaseResponse<Void> verifyAccountPassword(
-            @Valid @RequestBody AccountPasswordVerifyRequest request
-    ) {
-        accountService.verifyAccountPassword(request);
-        return BaseResponse.ok(null);
-    }
+	// 계좌 비밀번호 검증
+	@PostMapping("/password/verify")
+	public BaseResponse<Void> verifyAccountPassword(
+		@Valid @RequestBody AccountPasswordVerifyRequest request
+	) {
+		accountService.verifyAccountPassword(request);
+		return BaseResponse.ok(null);
+	}
+
+	// 계좌 생성
+	@PostMapping("/")
+	public BaseResponse<CreateAccountResponse> createAccount(
+		@Valid @RequestBody CreateAccountRequest request
+	) {
+		return BaseResponse.ok(accountService.createAccount(request));
+	}
 }
