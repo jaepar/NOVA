709d08684f8961db7f70b2b0f20b974361f4f89b
Jay Park <pjh6183@naver.com>
2026-06-01 15:49:09 +0900

    feat: 해당 유저 소유의 알림 삭제 메서드 구현

--- backend/src/main/java/woorifisa/project/backend/domain/user/repository/NotificationRepository.java
+++ backend/src/main/java/woorifisa/project/backend/domain/user/repository/NotificationRepository.java
@@ -1,7 +1,5 @@
 package woorifisa.project.backend.domain.user.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +11,6 @@ import woorifisa.project.backend.domain.user.entity.enums.NotificationType;
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
 	void deleteByUserAndType(User user, NotificationType type);
+
+	long deleteByNotificationIdAndUser_UserId(Long notificationId, Long userId);
 }
