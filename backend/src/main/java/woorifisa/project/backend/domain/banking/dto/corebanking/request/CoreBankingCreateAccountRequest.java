864f55e35e40e3e117e01c2e4740e6e1c945c416
Jo-dv <whgjsgh11@gmail.com>
2026-06-01 18:18:46 +0900

    feat: 온프레미스 계좌 생성 요청 dto

---
+++ backend/src/main/java/woorifisa/project/backend/domain/banking/dto/corebanking/request/CoreBankingCreateAccountRequest.java
@@ -0,0 +1,48 @@
+package woorifisa.project.backend.domain.banking.dto.corebanking.request;
+
+import woorifisa.project.backend.domain.banking.dto.request.AccountCreateRequest;
+import woorifisa.project.backend.domain.user.entity.User;
+
+public record CoreBankingCreateAccountRequest(
+        String accountType,
+        String accountName,
+        CustomerInfo customerInfo,
+        String job,
+        TransactionInfo transactionInfo,
+        Boolean hasForeignTax,
+        String accountPassword
+) {
+    public static CoreBankingCreateAccountRequest of(User user, AccountCreateRequest request) {
+        return new CoreBankingCreateAccountRequest(
+                request.accountType(),
+                request.accountName(),
+                new CustomerInfo(
+                        user.getName(),
+                        user.getEmail(),
+                        request.customerInfo().address(),
+                        request.customerInfo().addressDetail()
+                ),
+                request.job(),
+                new TransactionInfo(
+                        request.transactionInfo().purpose(),
+                        request.transactionInfo().source()
+                ),
+                request.hasForeignTax(),
+                request.accountPassword()
+        );
+    }
+
+    public record CustomerInfo(
+            String name,
+            String email,
+            String address,
+            String addressDetail
+    ) {
+    }
+
+    public record TransactionInfo(
+            String purpose,
+            String source
+    ) {
+    }
+}
