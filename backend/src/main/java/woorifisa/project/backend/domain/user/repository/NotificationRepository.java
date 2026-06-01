25b2c0a73e72e819615700cd315ee581a5b4196a
Jay Park <pjh6183@naver.com>
2026-06-01 17:35:48 +0900

    feat: 알림 조회 필요 메서드 구현

--- backend/src/main/java/woorifisa/project/backend/domain/user/repository/NotificationRepository.java
+++ backend/src/main/java/woorifisa/project/backend/domain/user/repository/NotificationRepository.java
@@ -1,5 +1,7 @@
 package woorifisa.project.backend.domain.user.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +15,6 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
 	void deleteByUserAndType(User user, NotificationType type);
 
 	long deleteByNotificationIdAndUser_UserId(Long notificationId, Long userId);
+
+	List<Notification> findAllByUser_UserIdOrderByCreatedAtDesc(Long userId);
 }
