8367939a73e7e9fdde072f69a5821a012b18921d
백민정 <ggamji09@naver.com>
2026-06-02 14:56:47 +0900

    feat: 지원서 제출 API 인증 설정 추가

--- backend/src/main/java/woorifisa/project/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/woorifisa/project/backend/global/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package woorifisa.project.backend.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -40,6 +41,7 @@ public class SecurityConfig {
                                 "/auth/signup",
                                 "/auth/email-verifications/**"
                         ).permitAll()
+                        .requestMatchers(HttpMethod.POST, "/jobs/*/applications").authenticated()
                         .requestMatchers(
                                 "/wallet/**",
                                 "/users/**",
