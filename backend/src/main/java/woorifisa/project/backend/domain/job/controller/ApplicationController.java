b35d0d1067d1c98e6eef2a162fa41eb08e57d52d
백민정 <ggamji09@naver.com>
2026-06-04 02:18:58 +0900

    feat : 지원 내역 조회 컨트롤러 추가

---
+++ backend/src/main/java/woorifisa/project/backend/domain/job/controller/ApplicationController.java
@@ -0,0 +1,38 @@
+package woorifisa.project.backend.domain.job.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import woorifisa.project.backend.domain.job.dto.response.ApplicationListResponse;
+import woorifisa.project.backend.domain.job.dto.response.PortfolioFileItem;
+import woorifisa.project.backend.domain.job.service.JobService;
+import woorifisa.project.backend.global.auth.security.SessionUserPrincipal;
+import woorifisa.project.backend.global.response.BaseResponse;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/applications")
+public class ApplicationController {
+
+	private final JobService jobService;
+
+	// 지원 내역 목록 조회
+	@GetMapping
+	public BaseResponse<ApplicationListResponse> findApplications(
+		@AuthenticationPrincipal SessionUserPrincipal principal
+	) {
+		return BaseResponse.ok(jobService.findApplications(principal.userId()));
+	}
+
+	// 지원 내역 세부 조회
+	@GetMapping("/{applicationId}/portfolios")
+	public BaseResponse<PortfolioFileItem> findApplicationPortfolio(
+		@AuthenticationPrincipal SessionUserPrincipal principal,
+		@PathVariable Long applicationId
+	) {
+		return BaseResponse.ok(jobService.findApplicationPortfolio(principal.userId(), applicationId));
+	}
+}
