8d69f53ba2439e205a36e3251e65fca3181923dd
백민정 <ggamji09@naver.com>
2026-06-03 17:14:47 +0900

    feat: 기존 등록 포트폴리오 조회 메서드 추가

--- backend/src/main/java/woorifisa/project/backend/domain/user/repository/ResumeRepository.java
+++ backend/src/main/java/woorifisa/project/backend/domain/user/repository/ResumeRepository.java
@@ -1,7 +1,13 @@
 package woorifisa.project.backend.domain.user.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import woorifisa.project.backend.domain.user.entity.Resume;
+import woorifisa.project.backend.domain.user.entity.User;
 
 public interface ResumeRepository extends JpaRepository<Resume, Long> {
+
+	// application_id가 NULL인 resume만 지원서 작성 화면의 기존 등록 포트폴리오로 사용한다.
+	List<Resume> findByUserAndApplicationIsNullOrderByResumeIdDesc(User user);
 }
