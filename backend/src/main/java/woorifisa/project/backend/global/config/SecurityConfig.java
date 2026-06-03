28f427f1f9beb587f6ea95fd08b2cdd704bc1648
백민정 <ggamji09@naver.com>
2026-06-04 02:19:26 +0900

    feat : 지원 내역 조회 API 인증 설정 추가

--- backend/src/main/java/woorifisa/project/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/woorifisa/project/backend/global/config/SecurityConfig.java
@@ -48,6 +48,7 @@ public class SecurityConfig {
                                 "/hospital/**",
                                 "/auth/logout",
                                 "/auth/me",
+                                "/applications/**",
                                 "/jobs/*/applications",
                                 "/jobs/applications/form"
                         ).authenticated()
