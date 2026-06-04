da3972e49150378af27f9a4ad48f1f4cf1b9f25f
백민정 <ggamji09@naver.com>
2026-06-04 17:44:25 +0900

    feat(job): 사용자 포트폴리오 URL 조회 추가

--- backend/src/main/java/woorifisa/project/backend/domain/user/repository/ResumeRepository.java
+++ backend/src/main/java/woorifisa/project/backend/domain/user/repository/ResumeRepository.java
@@ -1,6 +1,7 @@
 package woorifisa.project.backend.domain.user.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import woorifisa.project.backend.domain.user.entity.Resume;
@@ -9,4 +10,6 @@ import woorifisa.project.backend.domain.user.entity.User;
 public interface ResumeRepository extends JpaRepository<Resume, Long> {
 
 	List<Resume> findByUserOrderByResumeIdDesc(User user);
+
+	Optional<Resume> findByUserAndUrl(User user, String url);
 }
