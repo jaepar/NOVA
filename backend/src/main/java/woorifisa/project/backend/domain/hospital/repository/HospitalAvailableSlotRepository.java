3650908a2f85faa76509b2063ca2f5b44f462c7b
WOOJAE JO <whgjsgh11@gmail.com>
2026-06-09 15:50:49 +0900

    feat: 예약 시간 Repository

---
+++ backend/src/main/java/woorifisa/project/backend/domain/hospital/repository/HospitalAvailableSlotRepository.java
@@ -0,0 +1,15 @@
+package woorifisa.project.backend.domain.hospital.repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import woorifisa.project.backend.domain.hospital.entity.HospitalAvailableSlot;
+
+@Repository
+public interface HospitalAvailableSlotRepository extends JpaRepository<HospitalAvailableSlot, Long> {
+
+    Optional<HospitalAvailableSlot> findByHospitalHospitalIdAndAvailableAt(Long hospitalId, LocalDateTime availableAt);
+}
