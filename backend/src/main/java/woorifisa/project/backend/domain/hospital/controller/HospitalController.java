7b5921d325f31cbf39c35b9d2b19f557e0a383b4
WOOJAE JO <whgjsgh11@gmail.com>
2026-06-09 15:48:14 +0900

    feat: 병원 예약 Controller

--- backend/src/main/java/woorifisa/project/backend/domain/hospital/controller/HospitalController.java
+++ backend/src/main/java/woorifisa/project/backend/domain/hospital/controller/HospitalController.java
@@ -3,10 +3,13 @@ package woorifisa.project.backend.domain.hospital.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import woorifisa.project.backend.domain.hospital.dto.request.CreateReservationRequest;
 import woorifisa.project.backend.domain.hospital.dto.response.HospitalListResponse;
 import woorifisa.project.backend.domain.hospital.entity.enums.DepartmentType;
 import woorifisa.project.backend.domain.hospital.service.HospitalService;
@@ -26,5 +29,14 @@ public class HospitalController {
         @RequestParam(required = false) DepartmentType type
     ) {
         return BaseResponse.ok(hospitalService.findHospitals(type));
+    }
+
+    @PostMapping("/reservations")
+    public BaseResponse<Void> createReservation(
+        @AuthenticationPrincipal SessionUserPrincipal principal,
+        @RequestBody CreateReservationRequest request
+    ) {
+        hospitalService.createReservation(principal.userId(), request);
+        return BaseResponse.ok(null);
     }
 }
