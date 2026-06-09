52144fc4d57f12e7776928156593d2974fb063d8
WOOJAE JO <whgjsgh11@gmail.com>
2026-06-09 12:41:51 +0900

    feat: 병원 목록 Service

--- backend/src/main/java/woorifisa/project/backend/domain/hospital/service/HospitalService.java
+++ backend/src/main/java/woorifisa/project/backend/domain/hospital/service/HospitalService.java
@@ -1,9 +1,26 @@
 package woorifisa.project.backend.domain.hospital.service;
 
+import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import woorifisa.project.backend.domain.hospital.dto.response.HospitalListResponse;
+import woorifisa.project.backend.domain.hospital.entity.Hospital;
+import woorifisa.project.backend.domain.hospital.entity.enums.DepartmentType;
+import woorifisa.project.backend.domain.hospital.repository.HospitalRepository;
 
 @Service
 @RequiredArgsConstructor
 public class HospitalService {
+
+    private final HospitalRepository hospitalRepository;
+
+    public HospitalListResponse findHospitals(DepartmentType type) {
+        List<Hospital> hospitals = type == null
+            ? hospitalRepository.findAll()
+            : hospitalRepository.findAllByType(type);
+
+        return HospitalListResponse.from(hospitals);
+    }
 }
