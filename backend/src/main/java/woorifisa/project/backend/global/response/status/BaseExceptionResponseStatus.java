c1d91eebfedc53d8dc4284bc6c9e807a9a5e7b59
WOOJAE JO <whgjsgh11@gmail.com>
2026-06-10 09:58:35 +0900

    feat: 예약 취소 공통 응답 정의

--- backend/src/main/java/woorifisa/project/backend/global/response/status/BaseExceptionResponseStatus.java
+++ backend/src/main/java/woorifisa/project/backend/global/response/status/BaseExceptionResponseStatus.java
@@ -76,6 +76,9 @@ public enum BaseExceptionResponseStatus implements ResponseStatus {
      */
     HOSPITAL_NOT_FOUND("HOSPITAL-001", "병원 정보를 찾을 수 없습니다."),
     HOSPITAL_AVAILABLE_SLOT_NOT_FOUND("HOSPITAL-002", "예약 가능한 병원 시간이 아닙니다."),
+    HOSPITAL_RESERVATION_NOT_FOUND("HOSPITAL-003", "예약 정보를 찾을 수 없습니다."),
+    HOSPITAL_RESERVATION_ALREADY_CANCELED("HOSPITAL-004", "이미 취소된 예약입니다."),
+    HOSPITAL_RESERVATION_INVALID_ACTION("HOSPITAL-005", "지원하지 않는 예약 처리 요청입니다."),
 
     /**
      * admin
