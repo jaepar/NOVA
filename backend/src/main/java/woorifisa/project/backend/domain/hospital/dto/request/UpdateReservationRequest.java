5f97d797dc5f6827a757d4de0cbfe88ef8e1662d
WOOJAE JO <whgjsgh11@gmail.com>
2026-06-10 14:26:28 +0900

    refact: 요청 dto 예약 상태 변경 분기 추가

--- backend/src/main/java/woorifisa/project/backend/domain/hospital/dto/request/UpdateReservationRequest.java
+++ backend/src/main/java/woorifisa/project/backend/domain/hospital/dto/request/UpdateReservationRequest.java
@@ -1,6 +1,12 @@
 package woorifisa.project.backend.domain.hospital.dto.request;
 
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record UpdateReservationRequest(
-    String action
+    String action,
+    @JsonProperty("reserved_at")
+    LocalDateTime reservedAt
 ) {
 }
