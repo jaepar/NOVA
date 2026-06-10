6408d1fbb39e0cad48d4bebf0f5e837930b0e914
WOOJAE JO <whgjsgh11@gmail.com>
2026-06-10 10:03:15 +0900

    refact: 예약 확인 로직

--- backend/src/main/java/woorifisa/project/backend/domain/hospital/repository/ReservationRepository.java
+++ backend/src/main/java/woorifisa/project/backend/domain/hospital/repository/ReservationRepository.java
@@ -1,7 +1,11 @@
 package woorifisa.project.backend.domain.hospital.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import woorifisa.project.backend.domain.hospital.entity.Reservation;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    Optional<Reservation> findByReservationIdAndUserUserId(Long reservationId, Long userId);
 }
