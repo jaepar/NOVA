bd989ee95d45db4e1f3d8968e9c45ba5aec9a832
백민정 <ggamji09@naver.com>
2026-06-10 10:42:28 +0900

    refactor: 회원 정보 수정 API 요청 처리 추가

--- backend/src/main/java/woorifisa/project/backend/domain/user/dto/request/UpdateUserRequest.java
+++ backend/src/main/java/woorifisa/project/backend/domain/user/dto/request/UpdateUserRequest.java
@@ -1,9 +1,17 @@
 package woorifisa.project.backend.domain.user.dto.request;
 
+import jakarta.validation.constraints.Pattern;
+
 public record UpdateUserRequest(
 	String language,
 	String currentPassword,
+
+	@Pattern(
+		regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[^A-Za-z\\d]).{8,16}$",
+		message = "비밀번호는 영문, 숫자, 특수문자 포함 8~16자여야 합니다"
+	)
 	String newPassword,
+
 	String newPasswordConfirm,
 	Long deletePortfolioId
 ) {
