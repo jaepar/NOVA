6125e175e3a2da95ec79bbcb58bcb9f9ee4d61fb
Jo-dv <whgjsgh11@gmail.com>
2026-06-11 23:47:16 +0900

    refact: 상단 로고 제거

--- frontend/src/app/pages/main/MainHeaderBrand.tsx
+++ frontend/src/app/pages/main/MainHeaderBrand.tsx
@@ -1,11 +1,13 @@
-import headerBrandIcon from './assets/header-brand-icon.png'
-import headerBrandWordmark from './assets/header-brand-wordmark.png'
+import headerBrandWordmark from "./assets/header-brand-wordmark.png";
 
 export function MainHeaderBrand() {
   return (
     <div className="flex w-max shrink-0 items-center gap-3">
-      <img src={headerBrandIcon} alt="" className="h-8 w-8 shrink-0 object-contain" />
-      <img src={headerBrandWordmark} alt="NOVA" className="h-[18px] w-auto shrink-0 object-contain" />
+      <img
+        src={headerBrandWordmark}
+        alt="NOVA"
+        className="h-[18px] w-auto shrink-0 object-contain"
+      />
     </div>
-  )
+  );
 }
